d036645c0b6131788e5cf8429f23cd995a6a6b68
Henry Stoke <github@hmstoke.com>
2023-02-11 20:08:31 +0900

    Add error message

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { WebGPUSimulation } from "./webgpu";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 // const simulation = new CanvasSimulation(2000, canvas);
-WebGPUSimulation.from(400_000, canvas).then((simulation) => {
-    runSimulationInLoop(simulation);
-    (window as any).simulation = simulation;
-});
+WebGPUSimulation.from(400_000, canvas)
+    .then((simulation) => {
+        runSimulationInLoop(simulation);
+        (window as any).simulation = simulation;
+    })
+    .catch(() => {
+        const message = document.createElement("h4");
+        message.innerText =
+            "This app does not work without Web GPU - try running Chrome Canary or Firefox Nightly, and enabling Web GPU";
+        canvas.parentElement?.appendChild(message);
+    });
