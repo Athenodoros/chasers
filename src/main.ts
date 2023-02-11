29200b0e2d1ebfdfad288bf0b69bf00cb7caa947
Henry Stoke <github@hmstoke.com>
2023-02-11 20:05:05 +0900

    Remove top-level await for build

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { WebGPUSimulation } from "./webgpu";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 // const simulation = new CanvasSimulation(2000, canvas);
-const simulation = await WebGPUSimulation.from(400_000, canvas);
-runSimulationInLoop(simulation);
-(window as any).simulation = simulation;
+WebGPUSimulation.from(400_000, canvas).then((simulation) => {
+    runSimulationInLoop(simulation);
+    (window as any).simulation = simulation;
+});
